8eea499896f26f105f1ffaccd1964e41aebe9d51
Vilém Makovička <122599494+VilemMakovicka@users.noreply.github.com>
2024-07-04 18:02:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,8 @@
 function onLoad(){
-    hideAllCollapsibles();
-    assignCollapsibles();
 }
 
 function ChangeLocation(location){
     window.location = location;
-}
-
-function assignCollapsibles(){
-    var collapsiableButtons = document.getElementsByClassName("collapsible-header");
-
-    for (let i = 0; i < collapsiableButtons.length; i++) 
-    {
-        collapsiableButtons[i].addEventListener("click", function() 
-        {
-            var content = this.nextElementSibling;
-            let caret = collapsiableButtons[i].children[0].children[1];
-
-            if (content.style.maxHeight)
-            {
-                this.classList.remove("active");
-                content.style.maxHeight = null;
-                caret.src = 'Assets/Caret_down.png';
-            } 
-            else 
-            {
-                this.classList.add("active");
-                content.style.maxHeight = content.scrollHeight + "px";
-                caret.src= 'Assets/Caret_up.png';
-            }
-        });
-    }
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var headers = document.querySelectorAll('.collapsible-header');
-        headers.forEach(function(header) {
-            header.addEventListener('click', function() {
-                var target = document.getElementById(header.getAttribute('data-target'));
-                if (target.style.display === 'none' || target.style.display === '') {
-                    target.style.display = 'block';
-                    header.querySelector('.collapsible-header-caret').src = 'Assets/Caret_up.png';
-                } else {
-                    target.style.display = 'none';
-                    header.querySelector('.collapsible-header-caret').src = 'Assets/Caret_down.png';
-                }
-            });
-        });
-    });
-}
-
-function hideAllCollapsibles()
-{
-    var collapsiableButtons = document.getElementsByClassName("collapsible-header");
-
-    for (let i = 0; i < collapsiableButtons.length; i++) 
-    {
-        var content = collapsiableButtons[i].nextElementSibling;
-        content.style.maxHeight = null;
-    }
 }
 
 function changeTheme(type){
